2a9b846bc790dfc9b67193a0f57ef72d1aa1727a
elviraKarnak <karnak@elvirainfotech.com>
2026-04-06 13:55:20 +0530

    acf-option pages updated

--- wp-content/themes/astra-child/inc/custom_functions.php
+++ wp-content/themes/astra-child/inc/custom_functions.php
@@ -15,7 +15,7 @@ if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Header Settings',
-		'menu_title'	=> 'Footer',
+		'menu_title'	=> 'Header',
 		'parent_slug'	=> 'theme-general-settings',
 	));
 
