1e4847fc03a4c5796667f289d7c4268fa7ad02c3
elviraKarnak <karnak@elvirainfotech.com>
2026-04-06 13:54:14 +0530

    option pages updated

--- wp-content/themes/astra-child/inc/custom_functions.php
+++ wp-content/themes/astra-child/inc/custom_functions.php
@@ -14,7 +14,7 @@ if( function_exists('acf_add_options_page') ) {
 
 	
 	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
+		'page_title' 	=> 'Theme Header Settings',
 		'menu_title'	=> 'Footer',
 		'parent_slug'	=> 'theme-general-settings',
 	));
@@ -22,24 +22,12 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Footer Settings',
 		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Testimonials',
-		'menu_title'	=> 'Testimonials',
 		'parent_slug'	=> 'theme-general-settings',
 	));
 
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Additional Settings',
 		'menu_title'	=> 'Additional Fields',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-    acf_add_options_sub_page(array(
-		'page_title' 	=> 'Property Forms Settings',
-		'menu_title'	=> 'Property Forms',
 		'parent_slug'	=> 'theme-general-settings',
 	));
 
