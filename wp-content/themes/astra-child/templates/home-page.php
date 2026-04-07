203a854b23724664917bd72aa0f896c0a0f5bb57
nandita-paul25 <nandita.paul@elvirainfotech.com>
2026-04-07 12:16:28 +0530

    homepage html update
    four section html updated

--- wp-content/themes/astra-child/templates/home-page.php
+++ wp-content/themes/astra-child/templates/home-page.php
@@ -8,9 +8,480 @@ get_header(); ?>
 
      <!-- html -->
 
-    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/sample.jpg" alt="sample" height="100" width="200">
+    <!-- <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/sample.jpg" alt="sample" height="100" width="200">
 
-Test homepage
+Test homepage -->
+
+
+  <header class="main-header">
+    <div class="container">
+
+      <!-- LEFT MENU -->
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Book</a>
+        <a href="#">Membership</a>
+        <a href="#">Blog</a>
+      </nav>
+
+      <!-- LOGO -->
+      <div class="logo">
+        <a href="index.html"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/site-logo.svg" alt="site logo"></a>
+      </div>
+
+      <!-- RIGHT SIDE -->
+      <div class="right">
+
+        <!-- LANGUAGE DROPDOWN -->
+        <div class="lang-dropdown">
+          <div class="selected">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/usa-flag.svg" alt="usa flag">
+            <span>English</span>
+          </div>
+
+          <ul>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/usa-flag.svg" alt="usa flag"> English
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/france.webp" alt="france"> French
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/spain.webp" alt="spain"> Spanish
+            </li>
+          </ul>
+        </div>
+
+        <!-- BUTTON -->
+        <div class="btn_wrapper">
+          <a href="#" class="btn">Contact Us <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg" alt="button arrow"></a>
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </header>
+
+
+  <section class="hero-section">
+    <div class="container">
+      <div class="row align-items-center">
+
+        <!-- LEFT -->
+        <div class="col-lg-7 text-center">
+          <h1>
+            You Were Never Meant to Sell Your Life <br>
+            One Hour at a Time.
+          </h1>
+          <div class="grad-border">
+            <p>Human and AI working together for a truthful, balanced future</p>
+          </div>
+          <p>
+            Where Human Intelligence meets Artificial Intelligence to create a truth-based, conscious, and scalable
+            future.
+          </p>
+          <div class="btns d-flex justify-content-center gap-3">
+            <div class="btn_wrapper">
+              <a href="#" class="btn btn2">Read the Book <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                  alt="button arrow"></a>
+            </div>
+            <div class="btn_wrapper">
+              <a href="#" class="btn btn2">Listen to the Audio <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                  alt="button arrow"></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- RIGHT -->
+        <div class="col-lg-5 text-end books">
+          <img src="assets/images/books.webp" alt="books">
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <section class="truth-section">
+    <div class="container">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-lg-4 text-center ">
+          <!-- LEFT -->
+          <div class="left">
+            <div class="icon-text"><img src="assets/images/book-icon.svg" alt="Book Icon"><span>In One System</span>
+            </div>
+
+
+            <h2>
+              The Truth You Feel... But Can’t Explain
+            </h2>
+
+            <p>
+              The system we live in was not built for balance. It was shaped by Greed, Fear, and Control. And whether we
+              realize it or not… we are all living inside it.
+            </p>
+
+            <!-- BUTTON -->
+            <div class="btn_wrapper">
+              <a href="#" class="btn">Discover the Truth <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                  alt="button arrow"></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- RIGHT -->
+        <div class="col-lg-4">
+
+          <div class="right">
+
+            <div class="content">
+              <div class="number">01</div>
+              <div>
+                <h3>Time for Money</h3>
+                <p>Your life is measured in hours sold… not value lived.</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <div class="number">02</div>
+              <div>
+                <h3>Centralized Control</h3>
+                <p>Decisions are made far from the people they affect.</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <div class="number">03</div>
+              <div>
+                <h3>The Disconnection</h3>
+                <p>We’ve lost connection to truth, purpose, and each other.</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="split-hero">
+    <div class="container">
+
+      <!-- TOP LEFT / RIGHT -->
+      <div class="row align-items-start top">
+
+        <!-- LEFT -->
+        <div class="col-lg-6 text-start">
+          <h2>GFC</h2>
+          <p>Greed · Fear · Control</p>
+        </div>
+
+        <!-- RIGHT -->
+        <div class="col-lg-6 text-end">
+          <h2>Hum<span>AIT</span></h2>
+          <p>Human Intelligence and Artificial Intelligence in Unity</p>
+        </div>
+
+      </div>
+
+      <!-- CENTER CONTENT -->
+      <div class="row justify-content-center text-center center-content">
+        <div class="col-lg-8">
+          <h3>A New System for Humanity Has Begun</h3>
+
+          <p>
+            Humanity stands at a turning point. One path continues the cycle of Greed, Fear, and Control. The other
+            leads to <span>Truth, Balance, and Unity.</span>
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+
+  <section class="architecture-section">
+    <div class="container">
+      <div class="row align-items-center content-gap">
+
+        <!-- LEFT IMAGE -->
+        <div class="col-lg-5 mb-4 mb-lg-0">
+          <div class="image-box">
+            <img src="assets/images/architecture-image.webp" alt="Architecture Inage">
+          </div>
+        </div>
+
+        <!-- RIGHT CONTENT -->
+        <div class="col-lg-5">
+          <div class="icon-text"><img src="assets/images/book-icon.svg" alt="Book Icon"><span>Introduction to
+              HumAIT</span>
+          </div>
+
+
+          <h2>The HumAIT Architecture</h2>
+
+          <p>
+            Discover the foundational pillars of the HumAIT system, designed to bring
+            balance and unity to humanity. This new architecture is built on three
+            transformative systems that can guide us toward a higher purpose and a unified future.
+          </p>
+          <div class="btn_wrapper">
+            <a href="#" class="btn">Explore the System <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                alt="button arrow"></a>
+          </div>
+          <!-- ICONS -->
+          <div class="icons">
+
+            <div class="icon-box">
+              <img src="assets/images/truth-system.svg" alt="Truth System">
+              <p>Truth System</p>
+            </div>
+
+            <div class="icon-box">
+              <img src="assets/images/sovereign-system.svg" alt="Sovereign System">
+              <p>Sovereign System</p>
+            </div>
+
+            <div class="icon-box">
+              <img src="assets/images/evolution-system.svg" alt="Evolution System">
+              <p>Evolution System</p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="journey-section">
+    <div class="container text-center">
+      <div class="icon-text"><img src="assets/images/book-icon.svg" alt="Book Icon"><span>How it Works</span>
+      </div>
+
+      <h2>Your Journey Into HumAIT Begins Here</h2>
+
+      <div class="row mt-5">
+
+        <!-- CARD 1 -->
+        <div class="col-lg-4 mb-4">
+          <div class="card-box">
+            <h3>01.</h3>
+            <h4>See The Truth</h4>
+            <p>
+              Before anything changes, you must see clearly. Understand how the system shapes your choices, and your
+              life.
+            </p>
+            <div class="btn_wrapper">
+              <a href="#" class="btn">Discover the Truth <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                  alt="button arrow"></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- CARD 2 -->
+        <div class="col-lg-4 mb-4">
+          <div class="card-box">
+            <h3>02.</h3>
+            <h4>Start With The Book</h4>
+            <p>
+              This is where everything begins. The HumAIT Movement is not just a book - it is the blueprint for a new
+              system.
+            </p>
+            <div class="btn_wrapper">
+              <a href="#" class="btn">Start Reading <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg" alt="button arrow"></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- CARD 3 -->
+        <div class="col-lg-4 mb-4">
+          <div class="card-box">
+            <h3>03.</h3>
+            <h4>Become Part of the System</h4>
+            <p>
+              Knowledge alone is not enough. Join the system and become part of its evolution.
+            </p>
+            <div class="btn_wrapper">
+              <a href="#" class="btn">Join HumAIT <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg" alt="button arrow"></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section class="books-section">
+    <div class="container text-center">
+      <div class="icon-text"><img src="assets/images/book-icon.svg" alt="Book Icon"><span>Our Books</span>
+      </div>
+      <h2>Start with The Foundation</h2>
+
+      <div class="row mt-5">
+
+        <!-- BOOK 1 -->
+        <div class="col-lg-4 mb-4">
+          <div class="book-card">
+            <a href="#"><img src="assets/images/religion-of-love.webp" alt="Religion of Love"></a>
+
+            <h4>Religion of Love</h4>
+            <p>
+              Religion of Love - The True United Nation calls humanity back to unity,
+              compassion, and dignity.
+            </p>
+
+            <a href="#" class="btn3">Get the Book <img src="assets/images/btn3-arrow.svg" alt="arrow"></a>
+          </div>
+        </div>
+
+        <!-- BOOK 2 -->
+        <div class="col-lg-4 mb-4">
+          <div class="book-card">
+            <a href="#"><img src="assets/images/HumAIT.webp" alt="HumAIT"></a>
+
+            <h4>HumAIT</h4>
+            <p>
+              The HumAIT Movement unites people to build a sustainable future based
+              on unity and respect.
+            </p>
+
+            <a href="#" class="btn3">Get the Book <img src="assets/images/btn3-arrow.svg" alt="arrow"></a>
+          </div>
+        </div>
+
+        <!-- BOOK 3 -->
+        <div class="col-lg-4 mb-4">
+          <div class="book-card">
+            <a href="#"><img src="assets/images/time.webp" alt="Time"></a>
+
+            <h4>Time = Time</h4>
+            <p>
+              Your future is shaped by how you value time. Time reveals the cost of
+              every choice.
+            </p>
+
+            <a href="#" class="btn3">Get the Book <img src="assets/images/btn3-arrow.svg" alt="arrow"></a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+
+
+  <footer class="main-footer">
+    <div class="container">
+
+      <!-- TOP BAR -->
+      <div class="row align-items-center top-bar">
+
+        <div class="col-lg-3 col-12">
+          <div class="logo text-md-start text-center">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/site-logo.svg" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-9 col-12 text-end">
+          <nav class="menu">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Book</a>
+            <a href="#">Membership</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact Us</a>
+          </nav>
+        </div>
+
+      </div>
+
+      <hr>
+
+      <!-- CONTENT -->
+      <div class="row mt-5">
+
+        <!-- LEFT -->
+        <div class="col-lg-4 mb-4">
+          <h4>The Future Begins With You</h4>
+          <div class="footer-content">
+            <p>
+              This is not just a platform.
+              This is a new way of thinking.
+            </p>
+            <p>Human + AI - growing side by side.</p>
+            <p>The question is simple:</p>
+            <p>Will you be part of what comes next?</p>
+          </div>
+
+
+          <div class="social">
+            <span>f</span>
+            <span>x</span>
+            <span>in</span>
+          </div>
+        </div>
+
+        <!-- CENTER -->
+        <div class="col-lg-4 mb-4">
+          <div class="contact">
+            <ul>
+              <li>
+                <div class="btn_wrapper">
+                  <a href="#" class="btn">Join the Movement <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                      alt="button arrow"></a>
+                </div>
+              </li>
+              <li>
+                <div class="btn_wrapper">
+                  <a href="#" class="btn">Get the Book <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                      alt="button arrow"></a>
+                </div>
+              </li>
+              <li>
+                <div class="btn_wrapper">
+                  <a href="#" class="btn">Listen to the Audio <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/button-arrow.svg"
+                      alt="button arrow"></a>
+                </div>
+              </li>
+            </ul>
+
+
+
+          </div>
+        </div>
+
+        <!-- RIGHT -->
+        <div class="col-lg-4">
+          <h4>Be Part of What Comes Next</h4>
+          <p>Get early access to ideas, insights, and the evolution of HumAIT.</p>
+
+          <div class="newsletter">
+            <input type="text" placeholder="Your email address">
+            <button><img src="assets/images/newsletter-arrow.svg" alt="newsletter arrow"></button>
+          </div>
+          <p>No spam. No noise. Just truth.</p>
+        </div>
+
+      </div>
+
+      <!-- BOTTOM -->
+      <div class="bottom d-flex justify-content-between mt-5">
+        <p>© Copyright 2025 All rights reserved.</p>
+        <p>Designed & Developed by</p>
+      </div>
+
+    </div>
+  </footer>
+
+
 
 
 <?php get_footer(); ?>
