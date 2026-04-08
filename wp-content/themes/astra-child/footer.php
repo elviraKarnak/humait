d081ea9d2851d69839ade9c369090750cdf228b5
nandita-paul25 <nandita.paul@elvirainfotech.com>
2026-04-08 11:08:14 +0530

    footer update

--- wp-content/themes/astra-child/footer.php
+++ wp-content/themes/astra-child/footer.php
@@ -15,22 +15,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<?php astra_content_bottom(); ?>
+<?php // astra_content_bottom(); ?>
 	</div> <!-- ast-container -->
 	</div><!-- #content -->
 <?php
-	astra_content_after();
+	// astra_content_after();
 
-	astra_footer_before();
+	// astra_footer_before();
 
-	astra_footer();
+	// astra_footer();
 
-	astra_footer_after();
+	// astra_footer_after();
 ?>
 	</div><!-- #page -->
 <?php
-	astra_body_bottom();
-	wp_footer();
+	// astra_body_bottom();
+	// wp_footer();
 ?>
 
 
