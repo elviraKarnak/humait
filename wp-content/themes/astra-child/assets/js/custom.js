72f3817ee2edab766dbd111feeebfdee5437dc0a
nandita-paul25 <nandita.paul@elvirainfotech.com>
2026-04-08 11:15:02 +0530

    Initial custom js update

---
+++ wp-content/themes/astra-child/assets/js/custom.js
@@ -0,0 +1,39 @@
+// Menu toggle
+
+const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  const closeBtn = document.querySelector(".menu-close");
+
+  // Open menu
+  toggle.addEventListener("click", () => {
+    menu.classList.add("active");
+  });
+
+  // Close menu
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+
+
+// language dropdown
+
+const dropdown = document.querySelector(".lang-dropdown");
+const selected = dropdown.querySelector(".selected");
+const items = dropdown.querySelectorAll("li");
+
+selected.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+});
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    selected.innerHTML = item.innerHTML;
+    dropdown.classList.remove("active");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove("active");
+  }
+});
