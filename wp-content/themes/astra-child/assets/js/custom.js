80c56b2172b023a6e7addc3ec9356b515358ee63
nandita-paul25 <nandita.paul@elvirainfotech.com>
2026-04-09 17:13:46 +0530

    css and js update

--- wp-content/themes/astra-child/assets/js/custom.js
+++ wp-content/themes/astra-child/assets/js/custom.js
@@ -1,6 +1,6 @@
-// Menu toggle
-
-const toggle = document.querySelector(".menu-toggle");
+$(document).ready(function () {
+  // Menu toggle
+  const toggle = document.querySelector(".menu-toggle");
   const menu = document.querySelector(".menu");
   const closeBtn = document.querySelector(".menu-close");
 
@@ -14,26 +14,37 @@ const toggle = document.querySelector(".menu-toggle");
     menu.classList.remove("active");
   });
 
+  // language dropdown
 
-// language dropdown
+  const dropdown = document.querySelector(".lang-dropdown");
+  const selected = dropdown.querySelector(".selected");
+  const items = dropdown.querySelectorAll("li");
 
-const dropdown = document.querySelector(".lang-dropdown");
-const selected = dropdown.querySelector(".selected");
-const items = dropdown.querySelectorAll("li");
-
-selected.addEventListener("click", () => {
-  dropdown.classList.toggle("active");
-});
-
-items.forEach(item => {
-  item.addEventListener("click", () => {
-    selected.innerHTML = item.innerHTML;
-    dropdown.classList.remove("active");
+  selected.addEventListener("click", () => {
+    dropdown.classList.toggle("active");
   });
-});
 
-document.addEventListener("click", (e) => {
-  if (!dropdown.contains(e.target)) {
-    dropdown.classList.remove("active");
-  }
-});
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      selected.innerHTML = item.innerHTML;
+      dropdown.classList.remove("active");
+    }); 
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove("active");
+    }
+  });
+
+  // add sticky header
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+      $(".main-header").addClass("sticky");
+    } else {
+      $(".main-header").removeClass("sticky");
+    }
+  });
+});;
