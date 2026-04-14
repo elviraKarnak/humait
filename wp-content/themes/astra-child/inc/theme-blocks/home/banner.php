1ec9745a1cd86a670281ad0e3c1af2580ea47e4a
nandita-paul25 <nandita.paul@elvirainfotech.com>
2026-04-14 16:43:40 +0530

    banner content update

--- wp-content/themes/astra-child/inc/theme-blocks/home/banner.php
+++ wp-content/themes/astra-child/inc/theme-blocks/home/banner.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $homeBannerTitle = get_field('title_hb_bl');
 $homeBannerDescription = get_field('description_hb_bl_1');
@@ -18,68 +18,74 @@ $buttonArrowWidth = $buttonArrow ? $buttonArrow['width'] : '11';
 
 ?>
 
-<?php if(!empty($homeBannerTitle) || !empty($homeBannerDescription) || !empty($homeBannerDescription_2) || !empty($homeBannerRightBtn) || !empty($homeBannerLeftBtn) || !empty($homeBannerBookImg) || !empty($homeBannerCoverImgUrl) ): ?>
-    <section class="hero-section"
-    style='background: url("<?php echo $homeBannerCoverImgUrl; ?>") no-repeat center/cover;'>
-    <div class="container">
-        <div class="row align-items-center hero-content">
+<?php if (!empty($homeBannerTitle) || !empty($homeBannerDescription) || !empty($homeBannerDescription_2) || !empty($homeBannerRightBtn) || !empty($homeBannerLeftBtn) || !empty($homeBannerBookImg) || !empty($homeBannerCoverImgUrl)): ?>
+    <section class="hero-section" style='background: url("<?php echo $homeBannerCoverImgUrl; ?>") no-repeat center/cover;'>
+        <div class="container">
+            <div class="top-sc">
+               <p> Another war.</p>
+               <p> Another market crash.</p>
+               <p> Another crisis.</p>
+               <p> Another wave of inflation.</p>
 
-        <!-- LEFT -->
-        <div class="col-lg-7 text-center">
-            <?php if(!empty($homeBannerTitle)): ?>
-                <h1><?php echo $homeBannerTitle; ?></h1>
-            <?php endif; ?>
-            <?php if(!empty($homeBannerDescription)): ?>
-                <div class="grad-border">
-                    <p><?php echo $homeBannerDescription; ?></p>
-                </div>
-            <?php endif; ?>
-            <?php if(!empty($homeBannerDescription_2)): ?>
-                <p><?php echo $homeBannerDescription_2; ?></p>      
-            <?php endif; ?>
+               <p> It doesn’t have to be this way.</p>
 
-            <?php if(!empty($homeBannerRightBtn) || !empty($homeBannerLeftBtn)): ?>
-                <div class="btns d-flex justify-content-center align-items-center flex-column flex-md-row gap-3">
-                    <?php if(!empty($homeBannerRightBtn)): ?>
-                        <div class="btn_wrapper btn_wrapper2">
-                            <a href="<?php echo $homeBannerRightBtn['url']; ?>" target='<?php echo $homeBannerRightBtn['target'] ? $homeBannerRightBtn['target'] : '_self'; ?>' class="btn btn2">
-                                <?php echo $homeBannerRightBtn['title']; ?> 
-                                <img
-                                src="<?php echo $buttonArrowUrl; ?>"
-                                height="<?php echo $buttonArrowHeight; ?>"
-                                width="<?php echo $buttonArrowWidth; ?>"
-                                alt="button arrow" >
-                            </a>
+               <p>A different future is possible.</p>
+            </div>
+            <div class="row align-items-center hero-content">
+
+                <!-- LEFT -->
+                <div class="col-lg-7 text-center">
+                    <?php if (!empty($homeBannerTitle)): ?>
+                        <h1><?php echo $homeBannerTitle; ?></h1>
+                    <?php endif; ?>
+                    <?php if (!empty($homeBannerDescription)): ?>
+                        <div class="grad-border">
+                            <p><?php echo $homeBannerDescription; ?></p>
                         </div>
                     <?php endif; ?>
-                    <?php if(!empty($homeBannerLeftBtn)): ?>
-                    <div class="btn_wrapper">
-                        <a href="<?php echo $homeBannerLeftBtn['url']; ?>" target='<?php echo $homeBannerLeftBtn['target'] ? $homeBannerLeftBtn['target'] : '_self'; ?>' class="btn">
-                            <?php echo $homeBannerLeftBtn['title']; ?> 
-                            <img
-                                    src="<?php echo $buttonArrowUrl; ?>"
-                                    height="<?php echo $buttonArrowHeight; ?>"
-                                    width="<?php echo $buttonArrowWidth; ?>"
-                                    alt="button arrow" >
-                        </a>
-                    </div>
+                    <?php if (!empty($homeBannerDescription_2)): ?>
+                        <p><?php echo $homeBannerDescription_2; ?></p>
                     <?php endif; ?>
-                
+
+                    <?php if (!empty($homeBannerRightBtn) || !empty($homeBannerLeftBtn)): ?>
+                        <div class="btns d-flex justify-content-center align-items-center flex-column flex-md-row gap-3">
+                            <?php if (!empty($homeBannerRightBtn)): ?>
+                                <div class="btn_wrapper btn_wrapper2">
+                                    <a href="<?php echo $homeBannerRightBtn['url']; ?>"
+                                        target='<?php echo $homeBannerRightBtn['target'] ? $homeBannerRightBtn['target'] : '_self'; ?>'
+                                        class="btn btn2">
+                                        <?php echo $homeBannerRightBtn['title']; ?>
+                                        <img src="<?php echo $buttonArrowUrl; ?>" height="<?php echo $buttonArrowHeight; ?>"
+                                            width="<?php echo $buttonArrowWidth; ?>" alt="button arrow">
+                                    </a>
+                                </div>
+                            <?php endif; ?>
+                            <?php if (!empty($homeBannerLeftBtn)): ?>
+                                <div class="btn_wrapper">
+                                    <a href="<?php echo $homeBannerLeftBtn['url']; ?>"
+                                        target='<?php echo $homeBannerLeftBtn['target'] ? $homeBannerLeftBtn['target'] : '_self'; ?>'
+                                        class="btn">
+                                        <?php echo $homeBannerLeftBtn['title']; ?>
+                                        <img src="<?php echo $buttonArrowUrl; ?>" height="<?php echo $buttonArrowHeight; ?>"
+                                            width="<?php echo $buttonArrowWidth; ?>" alt="button arrow">
+                                    </a>
+                                </div>
+                            <?php endif; ?>
+
+                        </div>
+                    <?php endif; ?>
                 </div>
-            <?php endif; ?>
-        </div>
-        <!-- RIGHT -->
-        <?php if(!empty($homeBannerBookImg)): ?>
-            <div class="col-lg-5 text-end books">
-                <img src="<?php echo $homeBannerBookImg['url']; ?>" 
-                alt="<?php echo $homeBannerBookImg['alt']; ?>" 
-                height="<?php echo $homeBannerBookImg['height']; ?>" 
-                width="<?php echo $homeBannerBookImg['width']; ?>">
+                <!-- RIGHT -->
+                <?php if (!empty($homeBannerBookImg)): ?>
+                    <div class="col-lg-5 text-end books">
+                        <img src="<?php echo $homeBannerBookImg['url']; ?>" alt="<?php echo $homeBannerBookImg['alt']; ?>"
+                            height="<?php echo $homeBannerBookImg['height']; ?>"
+                            width="<?php echo $homeBannerBookImg['width']; ?>">
+                    </div>
+
+                <?php endif; ?>
+
             </div>
-
-            <?php endif; ?>
-
         </div>
-    </div>
     </section>
 <?php endif; ?>
