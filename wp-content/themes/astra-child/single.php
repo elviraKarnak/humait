1b75d4dbed3b62a0748d81c1efddea92f7eeeefc
elviraKarnak <karnak@elvirainfotech.com>
2026-04-17 15:25:07 +0530

    single page template updated

--- wp-content/themes/astra-child/single.php
+++ wp-content/themes/astra-child/single.php
@@ -12,28 +12,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-get_header(); ?>
 
-<?php if ( astra_page_layout() === 'left-sidebar' ) { ?>
+get_header('inner');
+if (have_posts()):
+  while (have_posts()):
+    the_post(); ?>
 
-	<?php get_sidebar(); ?>
+    <main>
+      <div class="inner-container-wp">
+        <div class="container">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </main>
 
-<?php } ?>
-
-	<div id="primary" <?php astra_primary_class(); ?>>
-
-		<?php astra_primary_content_top(); ?>
-
-		<?php astra_content_loop(); ?>
-
-		<?php astra_primary_content_bottom(); ?>
-
-	</div><!-- #primary -->
-
-<?php if ( astra_page_layout() === 'right-sidebar' ) { ?>
-
-	<?php get_sidebar(); ?>
-
-<?php } ?>
-
-<?php get_footer(); ?>
+  <?php endwhile;
+endif;
+get_footer(); ?>
