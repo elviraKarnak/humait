387cae8e2e0dbab457b54e8fe90dcdaaa6d7374b
elviraKarnak <karnak@elvirainfotech.com>
2026-04-17 13:10:36 +0530

    git inner template intoduced

---
+++ wp-content/themes/astra-child/templates/inner-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header('inner');
+    if(have_posts()) :
+        while (have_posts() ) : the_post(); ?>
+
+      <main>
+        <?php the_content(); ?>
+      </main>
+
+      <?php endwhile; 
+    endif; 
+get_footer(); ?>
